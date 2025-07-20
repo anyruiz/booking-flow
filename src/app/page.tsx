d3fd8587de7b3f31c8c4bc0c159a2edebc4ee81a
anyruizd <anyruizd+gh@gmail.com>
2025-07-19 20:21:05 -0400

    fix: text color on dark mode

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="flex items-center justify-center min-h-screen p-4 w-screen">
+    <div className="flex items-center justify-center min-h-screen p-4 w-screen bg-gray-20">
       <main className="flex flex-col gap-8 items-center w-full">
         <h1 className="text-4xl font-bold">Booking flow project</h1>
         <p>By Any Ruiz</p>
